efd671b2ce365c78340133c415ccf1047a3f47ad
tommykey0925 <tommykey0925@gmail.com>
2026-03-05 21:40:01 +0900

    init

---
+++ renderer.js
@@ -0,0 +1,3 @@
+document.getElementById('node-version').textContent = window.versions.node();
+document.getElementById('chrome-version').textContent = window.versions.chrome();
+document.getElementById('electron-version').textContent = window.versions.electron();
